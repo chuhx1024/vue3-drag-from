3e553aa187d3edea8416c844791e81f0d9ab7b21
chuhx <chuhx1024@126.com>
2023-12-13 11:44:44 +0800

    feat: init common css

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import '@/assets/styles/index.scss'
 
 const app = createApp(App)
 
