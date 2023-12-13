0de06245b022a672fb4d55f658d4b6eba0bce024
chuhx <chuhx1024@126.com>
2023-12-13 11:38:51 +0800

    feat: Eslint + Prettier config set

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,24 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
+        'no-debugger': process.env.NODE_ENV === 'production' ? 1 : 0,
+        indent: [2, 4], // js 4个空格缩进
+        'vue/html-indent': [2, 4], // html 4个空格缩进
+        eqeqeq: 0, // 允许使用 == !=
+        'comma-dangle': [2, 'always-multiline'], // 对象 数据 分行显示的 结尾必须加 ','
+        'quote-props': 0,
+        'vue/multi-word-component-names': 0,
+    },
 }
